b4dec1adadcf92035c9fbe4b6c456a2653aca290
Emanuele Tozzato <etozzato@gmail.com>
2010-09-04 15:10:14 -0700

    no cache

--- config/initializers/caching.rb
+++ config/initializers/caching.rb
@@ -1,11 +1,11 @@
-require 'rack/cache'
-if Rails.env != 'development'
-  host = 'localhost'
-  port = 11211
-  ActionController::Dispatcher.middleware.use Rack::Cache,
-    :verbose     => true,
-    :metastore   => "memcached://#{host}:#{port}/",
-    :entitystore => "memcached://#{host}:#{port}/",
-    :allow_reload     => false,
-    :allow_revalidate => false
-end
+# require 'rack/cache'
+# if Rails.env != 'development'
+#   host = 'localhost'
+#   port = 11211
+#   ActionController::Dispatcher.middleware.use Rack::Cache,
+#     :verbose     => true,
+#     :metastore   => "memcached://#{host}:#{port}/",
+#     :entitystore => "memcached://#{host}:#{port}/",
+#     :allow_reload     => false,
+#     :allow_revalidate => false
+# end
