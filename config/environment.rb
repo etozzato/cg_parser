0ab632b2cd827f9bb93ebfbeeb118360bec0435c
Emanuele Tozzato <etozzato@gmail.com>
2010-09-05 16:10:33 -0700

    Rails is not defined on boot =_)

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
+gem_path = File.expand_path('~/.gems')
+ENV['GEM_PATH'] =  gem_path + ':/usr/lib/ruby/gems/1.9.1' if gem_path
+
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-ENV['GEM_PATH'] = '/home/etozzato/.gems:/usr/lib/ruby/gems/1.9.1' if Rails.env != 'development'
 
 require File.join(File.dirname(__FILE__), 'boot.rb')
 
