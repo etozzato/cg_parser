1c4dcebc7dacd05d4382edeeca2fa9b790beb7fb
Emanuele Tozzato <etozzato@gmail.com>
2010-09-07 22:08:21 -0700

    create a variable that exclude rails/cache for rake tasks

--- Rakefile
+++ Rakefile
@@ -1,3 +1,4 @@
+RAKE_CMD = true
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
--- config/initializers/caching.rb
+++ config/initializers/caching.rb
@@ -1,11 +1,13 @@
-require 'rack/cache'
-if Rails.env != 'development'
-  host = 'localhost'
-  port = 11211
-  ActionController::Dispatcher.middleware.use Rack::Cache,
-    :verbose     => true,
-    :metastore   => "memcached://#{host}:#{port}/",
-    :entitystore => "memcached://#{host}:#{port}/",
-    :allow_reload     => false,
-    :allow_revalidate => false
+unless defined?(RAKE_CMD)
+  require 'rack/cache'
+  if Rails.env != 'development'
+    host = 'localhost'
+    port = 11211
+    ActionController::Dispatcher.middleware.use Rack::Cache,
+      :verbose     => true,
+      :metastore   => "memcached://#{host}:#{port}/",
+      :entitystore => "memcached://#{host}:#{port}/",
+      :allow_reload     => false,
+      :allow_revalidate => false
+  end
 end
