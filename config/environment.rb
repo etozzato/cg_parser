0c3d70cca6ac66794c93a37fda5969eecf23427d
Emanuele Tozzato <etozzato@gmail.com>
2010-09-07 23:04:46 -0700

    trying to solve the gempath problem on dreamhost

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-gem_path = File.expand_path('~/.gems')
-ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
-ENV['GEM_PATH'] =  gem_path + ':/usr/lib/ruby/gems/1.9.1' if gem_path
+#gem_path = File.expand_path('~/.gems')
+#ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
+#ENV['GEM_PATH'] =  gem_path + ':/usr/lib/ruby/gems/1.9.1' if gem_path
 
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
@@ -10,5 +10,5 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
   config.database_configuration_file = 'settings/database.yml'
 end
-require 'will_paginate' unless defined?(RAKE_CMD)
+require 'will_paginate' #unless defined?(RAKE_CMD)
 require File.join(File.dirname(__FILE__), '../', 'settings/base.rb')
--- config/initializers/caching.rb
+++ config/initializers/caching.rb
@@ -1,13 +1,13 @@
-unless defined?(RAKE_CMD)
-  require 'rack/cache'
-  if Rails.env != 'development'
-    host = 'localhost'
-    port = 11211
-    ActionController::Dispatcher.middleware.use Rack::Cache,
-      :verbose     => true,
-      :metastore   => "memcached://#{host}:#{port}/",
-      :entitystore => "memcached://#{host}:#{port}/",
-      :allow_reload     => false,
-      :allow_revalidate => false
-  end
+#unless defined?(RAKE_CMD)
+require 'rack/cache'
+if Rails.env != 'development'
+  host = 'localhost'
+  port = 11211
+  ActionController::Dispatcher.middleware.use Rack::Cache,
+    :verbose     => true,
+    :metastore   => "memcached://#{host}:#{port}/",
+    :entitystore => "memcached://#{host}:#{port}/",
+    :allow_reload     => false,
+    :allow_revalidate => false
 end
+#end
