7d76610d7d2ba0ea1781ad989dc36f15489a4f77
Emanuele Tozzato <etozzato@trueinteractive.net>
2011-03-28 02:46:44 -0700

    gemfile update

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,9 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot.rb')
 
+require 'dalli'
+require 'rack/cache'
+
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
   config.middleware.use Rack::Cache,
