e73f0574e85e6a07d1b53169aa35befbf5c9211d
Emanuele Tozzato <etozzato@gmail.com>
2011-03-30 16:09:09 -0700

    map expetiments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,17 @@ class ApplicationController < ActionController::Base
 
   private
   def set_app_name
-    @app_name ||= request.env['SERVER_NAME'].gsub(/^www\./,'').gsub(/^stage\./,'').gsub(/\.us$/,'').clean
-    @app_url  ||= request.env['SERVER_NAME'].gsub(/^www\./,'')
-    raise StandardError.new("Try using a stage env url:\n\n" + `cat /etc/hosts | grep stage`) if Rails.env.development? && Settings::MAKERS[@app_name.to_sym].nil?
+    if Rails.env.development?
+      @app_name ||= 'mybmw'
+      @app_url  ||= 'localhost'
+    else
+      @app_name ||= request.env['SERVER_NAME'].gsub(/^www\./,'').gsub(/^stage\./,'').gsub(/\.us$/,'').clean
+      @app_url  ||= request.env['SERVER_NAME'].gsub(/^www\./,'')
+    end
+    #raise StandardError.new("Try using a stage env url:\n\n" + `cat /etc/hosts | grep stage`) if Rails.env.development? && Settings::MAKERS[@app_name.to_sym].nil?
     @price_filter ||= {}
     @price_range ||= Post.first(:select => 'MAX(price) AS max, MIN(price) AS min', :conditions => ["maker = ?", @app_name])
-    @price_filter[:abs_min], @price_filter[:abs_max] = @price_range.min, @price_range.max
+    @price_filter[:abs_min], @price_filter[:abs_max] = @price_range.min, [@price_range.max.to_i, 200000].max
     @price_filter[:min], @price_filter[:max] = (params[:price_min]||@price_range.min), (params[:price_max]||@price_range.max)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :posts, :collection => {:search => :get}
   map.root :controller => 'posts'
-
+  
+  map.usmap '/cars-map-usa', :controller => 'maps'
+  
 end
