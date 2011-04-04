84fbe62b56a36906e239744718d1264a5bf14d21
Emanuele Tozzato <etozzato@trueinteractive.net>
2011-04-03 22:19:44 -0700

    V 1.5

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,3 +34,8 @@ function init_sliders(min_v, max_v, min, max){
       }
     });
 }
+
+function MM_jumpMenu(targ,selObj,restore){
+	eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+	if (restore) selObj.selectedIndex=0;
+}
